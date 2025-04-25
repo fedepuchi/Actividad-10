f4c2361afb6cd936af01db6531d80aaf9b39d07c
Federico Gis. <gismondifederico@gmail.com>
2025-04-25 00:33:47 -0500

    Changes to be committed: 	modified:   Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,8 @@ import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.regex.*;
 
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
